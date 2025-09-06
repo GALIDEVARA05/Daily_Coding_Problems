cb73981c835d9238374d3863ad3b5b80620ed1c6
Galidevara Venkata Lokesh <galidevaravenkatalokesh05@gmail.com>
2025-09-06 21:22:50 +0530

    Updated of Day-1

--- Code_1.py
+++ Code_1.py
@@ -1,9 +1,9 @@
 #palindrome
 # palindrome is a word that reads from front and back the same
-User_Input=input("Enter a Word:").upper()
+User_Input=input("Enter a Word:").lower()
 l=0
 r=len(User_Input)-1
-for j in range((len(User_Input)//2)+1): 
+for j in range(len(User_Input)//2): 
     if l<r and l!=r:
         if User_Input[j] == User_Input[r]:
             l+=1
@@ -11,5 +11,16 @@ for j in range((len(User_Input)//2)+1):
         else:
             print("Not a Palindrome")
             break
-    else:
-        print("The Given Word Is a Palindrome")
+else:
+    print("The Given Word Is a Palindrome")
+    
+#changed the simpler code version
+# User_Input=input("Enter a Word:")
+# l=0
+# r=len(User_Input)-1
+# for j in range((len(User_Input)//2)): 
+#         if User_Input[j] != User_Input[r-j]:
+#             print("Not a Palindrome")
+#             break
+# else:
+#     print("The Given Word Is a Palindrome")
